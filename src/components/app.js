1f4f397bdfc09b457f875f71baa4697dd43bab5b
Zach Schwartz <zischwartz@gmail.com>
2017-01-23 15:08:08 -0500

    tiny change

--- src/components/app.js
+++ src/components/app.js
@@ -21,7 +21,7 @@ class App extends React.Component {
         <p><strong>Women's Marches on January 21 2017</strong></p>
         <p><a href="https://twitter.com/womensmarch">Get Involved!</a></p>
         <p>This map shows the high estimate for the size of each crowd based on <a href="https://docs.google.com/spreadsheets/d/1xa0iLqYKz8x9Yc_rfhtmSOJQ2EGgeUVjvV4A8LsIaxY/edit#gid=0">this spreadsheet</a> which is being collected by Prof <a href="https://twitter.com/djpressman">Jeremy Pressman</a> and others. </p>
-        <p>Map created by <a href="https://twitter.com/zischwartz">Zach Schwartz</a> and the <a href="https://github.com/zischwartz/womensmarches">source code is available here</a>.</p>
+        <p>By <a href="https://twitter.com/zischwartz">Zach Schwartz</a> and the <a href="https://github.com/zischwartz/womensmarches">source code is available here</a>.</p>
         <p className="right"><a href="#" onClick={()=> this.setState({show_info:false})}>❌ Close This</a></p>
       </div>
       <Map/>
