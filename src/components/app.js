823c027d35fabfc6cdc293886e36fe215b137944
Zach Schwartz <zischwartz@gmail.com>
2017-01-24 09:35:16 -0500

    changed info pane to reflect genna's suggestions

--- src/components/app.js
+++ src/components/app.js
@@ -18,10 +18,10 @@ class App extends React.Component {
     let info_style = this.state.show_info ? {display:"block"} : {display:"none"}
     return <div id="app">
       <div id="info" style={info_style}>
-        <p><strong><a href="https://twitter.com/womensmarch">Women's Marches</a> on January 21 2017</strong></p>
-        <p>This map shows the high estimate for the size of each crowd based on <a href="https://docs.google.com/spreadsheets/d/1xa0iLqYKz8x9Yc_rfhtmSOJQ2EGgeUVjvV4A8LsIaxY/edit#gid=0">this spreadsheet</a> which is being collected by Prof <a href="https://twitter.com/djpressman">Jeremy Pressman</a> and others. </p>
+        <a className='close' href="#" onClick={()=> this.setState({show_info:false})}>X</a>
+        <div><strong>Worldwide <a href="https://twitter.com/womensmarch">Women's Marches</a> on January 21 2017</strong></div>
+        <p>This map shows the high estimate for the size of each crowd based on <a href="https://docs.google.com/spreadsheets/d/1xa0iLqYKz8x9Yc_rfhtmSOJQ2EGgeUVjvV4A8LsIaxY/edit#gid=0">this spreadsheet</a> which is being collected by Professors <a href="https://twitter.com/djpressman">Jeremy Pressman</a>, <a href="https://twitter.com/EricaChenoweth">Erica Chenoweth</a>, and others</p>
         <p>By <a href="https://twitter.com/zischwartz">Zach Schwartz</a> | <a href="https://github.com/zischwartz/womensmarches">Source Code</a></p>
-        <p className="right"><a href="#" onClick={()=> this.setState({show_info:false})}>❌ Close This</a></p>
       </div>
       <Map/>
     </div>
