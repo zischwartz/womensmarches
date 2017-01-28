5af7aede54ec7a9132d6836d7bdf010235942037
Zach Schwartz <zischwartz@gmail.com>
2017-01-28 14:20:36 -0500

    updated data with latest from spreadsheet, build

--- src/components/app.js
+++ src/components/app.js
@@ -22,6 +22,7 @@ class App extends React.Component {
         <div className='title'><strong>Worldwide <a href="https://twitter.com/womensmarch">Women's Marches</a> on January 21 2017</strong></div>
         <p>This map shows the high estimate for the size of each crowd based on <a href="https://docs.google.com/spreadsheets/d/1xa0iLqYKz8x9Yc_rfhtmSOJQ2EGgeUVjvV4A8LsIaxY/edit#gid=0">this spreadsheet</a> which is being collected by Professors <a href="https://twitter.com/djpressman">Jeremy Pressman</a>, <a href="https://twitter.com/EricaChenoweth">Erica Chenoweth</a>, and others</p>
         <p>By <a href="https://twitter.com/zischwartz">Zach Schwartz</a> | <a href="https://github.com/zischwartz/womensmarches">Source Code</a></p>
+        <p className="small">Last updated: Sat Jan 28 2017 14:12:49 GMT-0500 (EST)</p>
       </div>
       <Map/>
     </div>
